c1327ed869077a233718a734378d7eb3af38a8e5
JanKnapen <44979696+JanKnapen@users.noreply.github.com>
2020-12-18 11:38:18 +0100

    Day 1-3

---
+++ Day_01/main.py
@@ -0,0 +1,48 @@
+def part1():
+    pos = (0, 0)
+    dir = (0, 1)
+
+    for com in coms:
+        if com[0] == 'L':
+            dir = (-1*dir[::-1][0], dir[::-1][1])
+        if com[0] == 'R':
+            dir = (dir[::-1][0], -1*dir[::-1][1])
+        dis = int(com[1:len(com)])
+        pos = (pos[0]+dis*dir[0], pos[1]+dis*dir[1])
+
+    return abs(pos[0]) + abs(pos[1])
+
+def part2():
+    pos = (0,0)
+    dir = (0,1)
+    visited = [(0,0)]
+
+    class Found(Exception): pass
+    try:
+        for com in coms:
+            if com[0] == 'L':
+                dir = (-1*dir[::-1][0], dir[::-1][1])
+            if com[0] == 'R':
+                dir = (dir[::-1][0], -1*dir[::-1][1])
+            dis = int(com[1:len(com)])
+            newpos = (pos[0]+dis*dir[0], pos[1]+dis*dir[1])
+            visited.remove(pos)
+            for x in range(abs(newpos[0] - pos[0]) + 1):
+                for y in range(abs(newpos[1] - pos[1]) + 1):
+                    temppos = (pos[0]+dir[0]*x, pos[1]+dir[1]*y)
+                    if temppos in visited:
+                        visited.append(temppos)
+                        raise Found
+                    visited.append(temppos)
+            pos = newpos
+    except Found:
+        return abs(visited[-1][0]) + abs(visited[-1][1])
+
+
+with open("input1.txt") as f:
+    lines = f.readlines()
+
+coms = lines[0].split(", ")
+
+print("Answer part 1:", part1())
+print("Answer part 2:", part2())
---
+++ Day_02/main.py
@@ -0,0 +1,40 @@
+def result(grid, pos):
+    code = ""
+
+    for line in lines:
+        for letter in line:
+            if letter == 'U':
+                newpos = (pos[0], pos[1] - 1)
+            elif letter == 'D':
+                newpos = (pos[0], pos[1] + 1)
+            elif letter == 'R':
+                newpos = (pos[0] + 1, pos[1])
+            elif letter == 'L':
+                newpos = (pos[0] - 1, pos[1])
+            if grid[newpos[0]][newpos[1]] != 0:
+                pos = newpos
+        int = grid[pos[0]][pos[1]]
+        char = str(grid[pos[0]][pos[1]])
+        if int == 10:
+            char = 'A'
+        elif int == 11:
+            char = 'B'
+        elif int == 12:
+            char = 'C'
+        elif int == 13:
+            char = 'D'
+        code += char
+
+    return code
+
+
+with open("input2.txt") as f:
+    lines = f.readlines()
+
+pos = (2, 2)
+grid = [[0, 0, 0, 0, 0], [0, 1, 4, 7, 0], [0, 2, 5, 8, 0], [0, 3, 6, 9, 0], [0, 0, 0, 0, 0]]
+print("Answer part 1:", result(grid, pos))
+
+pos = (3, 3)
+grid = [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 5, 0, 0, 0], [0, 0, 2, 6, 10, 0, 0], [0, 1, 3, 7, 11, 13, 0], [0, 0, 4, 8, 12, 0, 0], [0, 0, 0, 9, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]
+print("Answer part 2:", result(grid, pos))
